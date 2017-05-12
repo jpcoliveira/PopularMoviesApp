13f7582397bceb293004c7f34e80d0ee4ae5a307
Joao Oliveira <joaopaulo_391@hotmail.com>
2017-05-11 23:27:43 -0300

    correcoes

--- app/src/main/java/com/example/android/popularmoviesapp/interactors/DetailInteractorImpl.java
+++ app/src/main/java/com/example/android/popularmoviesapp/interactors/DetailInteractorImpl.java
@@ -2,9 +2,12 @@ package com.example.android.popularmoviesapp.interactors;
 
 import android.content.Context;
 
+import com.example.android.popularmoviesapp.domain.adapters.TrailerAdapter;
 import com.example.android.popularmoviesapp.interfaces.interactors.DetailInteractor;
 import com.example.android.popularmoviesapp.model.MovieModel;
 import com.example.android.popularmoviesapp.domain.network.MovieDetailAsyncTask;
+import com.example.android.popularmoviesapp.model.ReviewModel;
+import com.example.android.popularmoviesapp.model.TrailerModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,10 +48,40 @@ public class DetailInteractorImpl implements DetailInteractor {
             MovieModel _movie = getFavoriteMovie(movie);
 
             if (_movie == null) {
+                movie.setId(null);
                 movie.setFavorite(true);
+
+                List<ReviewModel> reviews = movie.getReviews();
+                List<TrailerModel> trailers = movie.getTrailers();
+
+                for (ReviewModel review : reviews) {
+                    review.setId(null);
+                    review.save();
+                }
+
+                for (TrailerModel trailer : trailers) {
+                    trailer.setId(null);
+                    trailer.save();
+                }
+                movie.setTrailers(TrailerModel.find(TrailerModel.class, ""));
+                movie.setReviews(ReviewModel.find(ReviewModel.class, ""));
                 movie.save();
+
             } else {
+
+                List<ReviewModel> reviews = _movie.getReviews();
+                List<TrailerModel> trailers = _movie.getTrailers();
+
+                for (ReviewModel review : reviews) {
+                    review.delete();
+                }
+
+                for (TrailerModel trailer : trailers) {
+                    trailer.delete();
+                }
+
                 _movie.delete();
+
                 movie.setFavorite(false);
             }
         } catch (Exception ex) {
